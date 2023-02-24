1c66fd32814c911d75bc7d5af8dcc48125ddd60f
RomanasV <romanasvenckus@gmail.com>
2023-02-24 11:24:19 +0200

    events loop

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -2,55 +2,55 @@ import ArrowIcon from '../ArrowIcon';
 import LargeButton from '../Partials/LargeButton';
 
 export default function Events() {
+  const eventsData = [
+    {
+      image: 'https://codeacademy.lt/wp-content/uploads/2022/10/MicrosoftTeams-image-20-300x300.png',
+      day: 10,
+      month: 'lap',
+      location: 'Online',
+      title: 'CodeAcademy Talents',
+      url: './#'
+    },
+    {
+      day: 5,
+      month: 'lap',
+      location: '2 Online',
+      title: '2 CodeAcademy Talents',
+      url: './#'
+    },
+    {
+      day: null,
+      month: 'spa',
+      location: '',
+      title: '3 CodeAcademy Talents',
+      url: './#'
+    },
+  ];
+
   return (
     <div className="events">
       <h2 className="section-title">Renginiai</h2>
 
       <div className="events-wrapper">
-        <div className="event">
-          <a href="#">
-            <div className="image-wrapper">
-              <img src="https://codeacademy.lt/wp-content/uploads/2022/10/MicrosoftTeams-image-20-300x300.png" />
-            </div>
-            <div className="event-info">
-              <div className="date">
-                <div className="day">10</div>
-                <div className="month">lap</div>
+        {eventsData.map((event, index) => (
+          <div key={index} className="event">
+            <a href={event.url}>
+              <div className="image-wrapper">
+                {event.image && <img src={event.image} alt={event.title} />}
               </div>
+              <div className="event-info">
+                <div className="date">
+                  {event.day && <div className="day">{event.day}</div>}
+                  <div className="month">{event.month}</div>
+                </div>
 
-              <span className="event-location">Online</span>
-              <h3 className="event-title">CodeAcademy Talents</h3>
-            </div>
-          </a>
-        </div>
-
-        <div className="event">
-          <a href="#">
-            <div className="event-info">
-              <div className="date">
-                <div className="day">10</div>
-                <div className="month">lap</div>
+                {event.location && <span className="event-location">{event.location}</span>}
+                <h3 className="event-title">{event.title}</h3>
               </div>
+            </a>
+          </div>
+        ))}
 
-              <span className="event-location">Online</span>
-              <h3 className="event-title">CodeAcademy Talents</h3>
-            </div>
-          </a>
-        </div>
-
-        <div className="event">
-          <a href="#">
-            <div className="event-info">
-              <div className="date">
-                <div className="day">10</div>
-                <div className="month">lap</div>
-              </div>
-
-              <span className="event-location">Online</span>
-              <h3 className="event-title">CodeAcademy Talents</h3>
-            </div>
-          </a>
-        </div>
       </div>
 
       <LargeButton text="Daugiau" url="https://www.google.com" />
