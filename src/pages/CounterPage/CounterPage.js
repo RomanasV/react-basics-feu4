096d9229974163211f0f44e56de8797ad577e87c
RomanasV <romanasvenckus@gmail.com>
2023-02-28 10:10:52 +0200

    cars task

--- src/pages/CounterPage/CounterPage.js
+++ src/pages/CounterPage/CounterPage.js
@@ -6,13 +6,15 @@ const CounterPage = () => {
   const initialCount = 5;
   const [count, setCount] = useState(initialCount);
 
-  const counterHandler = (num) => setCount(count + num);
+  const counterHandler = num => setCount(count + num);
+  const counterInputHandler = event => setCount(Number(event.target.value));
 
   let classes = count > 4 ? 'color-green' : 'color-red';
 
   return (
     <Container>
       <h1 className={classes}>{count}</h1>
+      <input type="number" min="1" max="10" value={count} onChange={counterInputHandler} />
       <button onClick={() => counterHandler(2)} disabled={count > 8}>+2</button>
       <button onClick={() => counterHandler(1)} disabled={count > 9}>+1</button>
       <button onClick={() => counterHandler(-1)} disabled={count < 2}>-1</button>
