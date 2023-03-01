b654935b5fda3f88118e7fa52b839b38565feccf
RomanasV <romanasvenckus@gmail.com>
2023-03-01 10:44:23 +0200

    car form

--- src/pages/CarsPage/CarsPage.js
+++ src/pages/CarsPage/CarsPage.js
@@ -10,7 +10,9 @@ const CarsPage = () => {
   const [engine, setEngine] = useState(engineTypes[0]);
   const [basePrice, setBasePrice] = useState(1000);
   const [mileage, setMileage] = useState(0);
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
   const [color, setColor] = useState(colors[0]);
+  const [image, setImage] = useState('');
 
   const [car, setCar] = useState(null);
   
@@ -23,19 +25,6 @@ const CarsPage = () => {
 
     return elements;
   };
-
-  // const engineOptionElements = engineTypes.map((type, index) => {
-  //   const optionText = type[0].toUpperCase() + type.slice(1);
-
-  //   return <option key={index} value={type}>{optionText}</option>;
-  // })
-
-  const colorOptionElements = colors.map((color, index) => {
-    const optionText = color[0].toUpperCase() + color.slice(1);
-
-    return <option key={index} value={color}>{optionText}</option>
-  });
-
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
@@ -60,15 +49,26 @@ const CarsPage = () => {
 
     // setCar(newCar);
 
-    setCar({ brand, model });
+    setCar({ brand, model, color, image, engine, basePrice, mileage });
   }
 
   const brandInputHandler = event => setBrand(event.target.value);
   const modelInputHandler = event => setModel(event.target.value);
   const engineSelectHandler = event => setEngine(event.target.value);
   const basePriceHandler = event => setBasePrice(Number(event.target.value));
-  const mileageHandler = event => setMileage(event.target.value);
-  const carSelectHandler = event => setColor(event.target.value);
+  const mileageHandler = event => setMileage(Number(event.target.value));
+  
+  const colorSelectHandler = event => {
+    const selectedElementColor = event.target.value;
+    setSelectedColor(selectedElementColor);
+
+    if (selectedElementColor !== 'other') {
+      setColor(selectedElementColor);
+    }
+  };
+  
+  const otherColorHandler = event => setColor(event.target.value);
+  const imageHandler = event => setImage(event.target.value);
 
   return (
     <Container>
@@ -139,17 +139,51 @@ const CarsPage = () => {
           <div className="form-control">
             <label htmlFor="car-color">Color:</label>
             
-            <select id="car-color" name="color" value={color} onChange={carSelectHandler}>
+            <select id="car-color" name="color" value={selectedColor} onChange={colorSelectHandler}>
               {optionElements(colors)}
             </select>
           </div>
         )}
 
-        <button type="submit">Submit</button>
+        {selectedColor === 'other' && (
+          <div className="form-control">
+            <label htmlFor="other-car-color">Other color:</label>
+            <input 
+              type="text" 
+              id="other-car-color" 
+              name="other-color" 
+              placeholder="Enter a color" 
+              onChange={otherColorHandler}
+              />
+          </div>
+        )}
+
+          <div className="form-control">
+            <label htmlFor="car-image">Car image:</label>
+            <input 
+              type="url" 
+              id="car-image" 
+              name="image" 
+              placeholder="Enter an image url" 
+              onChange={imageHandler}
+              value={image}
+              />
+          </div>
+        
+        {brand && model && engine && basePrice && mileage && color && image && <button type="submit">Submit</button>}
       </form>
+
       {car && (
         <div className='car-item'>
           <h2 className='car-name'>{car.brand} {car.model}</h2>
+          <img src={car.image} alt={`${car.brand} ${car.model}`} />
+          <h3>Car's info:</h3>
+          <ul>
+            <li>Color: {car.color}</li>
+            <li>Engine type: {car.engine}</li>
+            <li>Mileage: {car.mileage}</li>
+            <li>Base price: {car.basePrice}</li>
+          </ul>
         </div>
       )}
     </Container>
